958cec02e0a4b138696e06796ba5433deac00643
tecnolweb <141664255+tecnolweb@users.noreply.github.com>
2024-08-24 23:55:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+document.getElementById('indicacaoForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const nomeIndicador = document.getElementById('nomeIndicador').value;
+    const nomeIndicado = document.getElementById('nomeIndicado').value;
+    const telefoneIndicado = document.getElementById('telefoneIndicado').value;
+
+    const texto = `Indicação: óla estou indicando o senhor(a):\n\nNome do Indicador: ${nomeIndicador}\nNome da Pessoa Indicada: ${nomeIndicado}\nNúmero de Telefone: ${telefoneIndicado}`;
+
+    const encodedText = encodeURIComponent(texto);
+    const numeroWhatsApp = '5581989630005';
+    const whatsappUrl = `https://wa.me/${numeroWhatsApp}?text=${encodedText}`;
+
+    window.open(whatsappUrl, '_blank');
+});
